2612edfc0b4fc0407144150865d0904aeff0b4ea
virajmilinda <virajmilinda999@gmail.com>
2023-11-12 14:24:12 +0530

    Add files via upload
    Coding Ninja Problem 4

---
+++ SQLQuery_4.sql
@@ -0,0 +1,10 @@
+/*Problem Statement
+Print the genre and the maximum net profit among all the movies of that genre released in 2012 per genre.
+(Note: Download the dataset from console)
+
+1. Do not print any row where either genre or the net profit is empty/null.
+2. net_profit = Domestic + Worldwide - Budget
+3. Keep the names of the columns
+*/
+
+
